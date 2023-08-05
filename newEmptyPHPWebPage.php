8f3f5d3c4dcafaa6488d2ad23e04cbc6a02a8b80
bloes007 <bloes@outlook.es>
2023-08-05 08:55:45 -0500

    Repositorio PHP

--- newEmptyPHPWebPage.php
+++ newEmptyPHPWebPage.php
@@ -1,36 +1,18 @@
 <!DOCTYPE html>
-<?php
-$estudiante = array(array("Lau",5));
 
-?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>PRACTICANDO</title>
     </head>
     <body>
-        <table border=1 width=60% style="margin: 0 auto;" cellspacing="5">
-            <tr>
-                <th>NOMBRE</th>
-                <th>NOTA</th>
-                <th>DEF</th>
-            </tr>
-            <?php
-            foreach ($estudiante as $value) {
-                echo '<tr>';
-                echo "<td>$value[0]</td>";
-                if($value[1] > 3){
-                    echo '<td> Aprobo </td>';
-                }
-                else{
-                    echo '<td> No aprobo </td>';
-                }
-                echo "<td>$value[1]</td>";
-                echo '</tr>';
-                
-            }
-            ?>
+        <?php
+        function (){
             
+        }
+        ?>
 
+
+    
     </body>
 </html>
